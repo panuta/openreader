e29ee6fbc53ec51001487b1a759aa42d8a446f47
Tarongpong Jaratwuttiyakorn <kotaromaster@hotmail.com>
2012-06-25 14:27:22 +0700

    add localdatetime to utilities function

--- common/utilities.py
+++ common/utilities.py
@@ -11,6 +11,8 @@ from dateutil import tz
 from constants import THAI_MONTH_NAME, THAI_MONTH_ABBR_NAME
 
 def format_full_datetime(datetime):
+    localzone = tz.tzlocal()
+    datetime = datetime.astimezone(localzone)
     try:
         return unicode('%d %s %d เวลา %02d:%02d น.', 'utf-8') % (datetime.day, unicode(THAI_MONTH_NAME[datetime.month], 'utf-8'), datetime.year + 543, datetime.hour, datetime.minute)
     except:
