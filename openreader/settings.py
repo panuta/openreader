5f2ec6ef1c34b145f544f86087ba1bd32c970d2f
Ubuntu <ubuntu@ip-10-138-149-205.ap-southeast-1.compute.internal>
2012-10-29 06:47:57 +0000

    add ssl

--- openreader/settings.py
+++ openreader/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 WEBSITE_NAME = 'OpenReader'
-WEBSITE_DOMAIN = 'http://127.0.0.1:8000'
+WEBSITE_DOMAIN = 'https://127.0.0.1:9001'
 
 DATABASES = {
     'default': {
@@ -43,6 +43,11 @@ SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# SSL
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 MEDIA_ROOT = os.path.join(base_path, 'media/')
 MEDIA_URL = '/media/'
@@ -226,7 +231,7 @@ CELERYBEAT_SCHEDULE = {
     },
     # 'decide-on-first-month-test-every-10-seconds': {
     #     'task': 'tasks.send_notification_email_to_decide_on_first_month',
-    #     'schedule': timedelta(seconds=10),
+    #     'schedule': timedelta(seconds=30),
     # },
 }
 
