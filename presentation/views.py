6c120385ca4efc21a214ed6b5d53817a01ee8409
Tarongpong Jaratwuttiyakorn <kotaromaster@hotmail.com>
2013-03-15 10:26:00 +0700

    removed duplicate latest uploaded files in view documents page.

--- presentation/views.py
+++ presentation/views.py
@@ -429,7 +429,7 @@ def view_documents(request, organization_slug):
 
     shelves = get_permission_backend(request).get_viewable_shelves(request.user, organization)
     uploadable_shelves = get_permission_backend(request).get_uploadable_shelves(request.user, organization)
-    recent_publications = Publication.objects.filter(shelves__in=shelves).order_by('-uploaded')[:10]
+    recent_publications = Publication.objects.filter(shelves__in=shelves).distinct().order_by('-uploaded')[:10]
 
     return render(request, 'document/documents.html', {
         'organization': organization,
